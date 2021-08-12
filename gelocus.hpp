1be6ccd3f641909ca172a8dcdb19ad012ab0a6fe
Gunvir Ranu <mail@gunvirranu.com>
2021-08-12 18:40:54 -0400

    Impl position class in specific frame

--- gelocus.hpp
+++ gelocus.hpp
@@ -1,7 +1,18 @@
 #ifndef GELOCUS_HPP
 #define GELOCUS_HPP
 
+#include <cstddef>
+#include <iostream>
+#include <stdexcept>
+
+#ifndef GELOCUS_VEC3
+#error Must define `GELOCUS_VEC3` type
+#endif
+
 namespace gelocus {
+
+using std::size_t;
+using Vec = GELOCUS_VEC3;
 
 enum class Frame {
     J2000,
@@ -9,6 +20,28 @@ enum class Frame {
     TOD,
     PEF,
     ECEF
+};
+
+std::ostream& operator<<(std::ostream &os, Frame f);
+
+template <Frame F>
+class Position {
+public:
+    Vec vec;
+
+    explicit Position(Vec v) : vec(v) {}
+
+    const auto& operator()(size_t i) const {
+        return vec(i);
+    }
+
+    auto& operator()(size_t i) {
+        return vec(i);
+    }
+
+    friend std::ostream& operator<<(std::ostream &os, const Position &p) {
+        return os << "Position<" << F << "> { " << p(0) << ", " << p(1) << ", " << p(2) << " }";
+    }
 };
 
 std::ostream& operator<<(std::ostream &os, Frame f) {
